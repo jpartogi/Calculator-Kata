1c13a0ee0c8663bddb395273547b95742ff2dd93
Joshua Partogi <proscrumtrainer@live.com>
2015-07-23 16:45:13 +0800

    Update website on README

--- src/test/java/leanagile/CalculatorTest.java
+++ src/test/java/leanagile/CalculatorTest.java
@@ -33,5 +33,53 @@ public class CalculatorTest {
 		
 		assertThat(result, equalTo( 4 ));
 	}
+	
+	@Test
+	public void acceptsThree(){
+		Calculator calc = new Calculator();
+		
+		int result = calc.add("2,3,4");
+		
+		assertThat(result, equalTo( 9 ));
+	}
 
+	@Test(expected=IllegalArgumentException.class)
+	public void fourThrowsException(){
+		Calculator calc = new Calculator();
+		
+		int result = calc.add("1,2,3,4");
+	}
+	
+	@Test
+	public void ignoresNewLines(){
+		Calculator calc = new Calculator();
+		
+		int result = calc.add("2,\n3,4\n");
+		
+		assertThat(result, equalTo( 9 ));
+		
+		result = calc.add("1\n,3,\n4");
+		
+		assertThat(result, equalTo( 8 ));
+	}
+	
+	@Test
+	public void acceptsOtherDelimiters(){
+		Calculator calc = new Calculator();
+		
+		int result = calc.add("1;2,%3");
+		
+		assertThat(result, equalTo(6));
+		
+		result = calc.add("3:4,.5");
+		
+		assertThat(result, equalTo(12));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void negativesNotAllowed(){
+		Calculator calc = new Calculator();
+		
+		int result = calc.add("-1%:-2;,-3");
+	}
 }
